ca39a18faaf5d105248be385d27c57edf1e4477e
johan52824 <58148047+johan52824@users.noreply.github.com>
2021-11-19 00:18:38 -0500

    Add files via upload

---
+++ WEB II REPOSITORIO/01Introducion clases/index.js
@@ -0,0 +1,12 @@
+const { funcion1, variable } = require('./funcionesmat')
+
+
+let resultado =  funcion1(5, variable ,"sumar") ;
+console.log(resultado)
+
+// let numero =5
+
+// for ( let i=0;i<5; i++ )
+// {
+//     console.log(`Actualmente I vale ${i} `)
+// }
